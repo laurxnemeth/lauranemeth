383f039e22b2c9d2d57996b9800b310c18b38ae3
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-04-10 14:36:00 -0400

    under construction mode

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function addActiveClass(clickedId){
-    const topnav = document.getElementById("topnav")
-    for (let i = 0; i < topnav.children.length; i++){
-        topnav.children[i].classList.remove("active")
-    }
-    const element = document.getElementById(clickedId);
-    element.classList.add("active")
-}
+// function addActiveClass(clickedId){
+//     const topnav = document.getElementById("topnav")
+//     for (let i = 0; i < topnav.children.length; i++){
+//         topnav.children[i].classList.remove("active")
+//     }
+//     const element = document.getElementById(clickedId);
+//     element.classList.add("active")
+// }
