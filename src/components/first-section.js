8de705f5a8017e36e71cc5c5f5f8ae321edb5c0c
Laura Nemeth <lauranemeth.pr@gmail.com>
2021-11-23 20:38:59 -0500

    start responsiveness

--- src/components/first-section.js
+++ src/components/first-section.js
@@ -6,28 +6,31 @@ import React from 'react';
 export default function NameAndHeadshot() {
   const [modalShow, setModalShow] = React.useState(false);
 
-  return(
-    <div id="first-section">
-      <div id="top-section">
-        <h3 id="title">front-end engineer</h3>
-        <span class="sub" onClick={() => setModalShow(true)}>wanna chat? click here!</span>
-        {/* link opens module with contact me info*/}
-      </div>
-      <div id="name-and-headshot">
-        <div>
-          <h1 className="name">Laura</h1>
-          <h1 className="name">Nemeth</h1>
-        </div>
-        <div id="wrap">
-          <div id="decorative-circle"></div>
-          <img
-            className="headshot"
-            src={headshot}
-            alt="Headshot of Laura Nemeth, they stand in front of trees whilst wearing a colorful button up shirt"
-          />
-        </div>
-      </div>
-      <InfoModal show={modalShow} onHide={() => setModalShow(false)} />
-  </div>
-  );
+  return (
+		<div id="first-section">
+			<div id="top-section">
+				<h3 id="title">front-end engineer</h3>
+				<span class="sub" onClick={() => setModalShow(true)}>
+					wanna chat? click here!
+				</span>
+				{/* link opens module with contact me info*/}
+			</div>
+			<div id="name-and-headshot">
+				<div id="name-wrapper">
+					<h1 className="name birthstone-font">hi! I 'm</h1>
+					<h1 className="name birthstone-font">Laura</h1>
+					<h1 className="name birthstone-font">Nemeth</h1>
+				</div>
+				<div id="wrap">
+					<div id="decorative-circle"></div>
+					<img
+						className="headshot"
+						src={headshot}
+						alt="Headshot of Laura Nemeth, they stand in front of trees whilst wearing a colorful button up shirt"
+					/>
+				</div>
+			</div>
+			<InfoModal show={modalShow} onHide={() => setModalShow(false)} />
+		</div>
+	);
 };
