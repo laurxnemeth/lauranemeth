7f53f762f2fd61f3d14517a3005185d4b47cfd04
Laura Nemeth <lauranemeth.pr@gmail.com>
2022-09-14 18:33:19 -0400

    fix bugs hopefully

--- src/components/NameAndHeadshot.js
+++ src/components/NameAndHeadshot.js
@@ -6,9 +6,16 @@ export default function NameAndHeadshot() {
 		<div id="first-section">
 			<div id="name-and-headshot">
 				<div id="name-wrapper">
-					<h1 className="name">hello!</h1>
-					<h1 className="name">I am</h1>
-					<h1 className="name" id="me">Laura Nemeth</h1>
+					<h1 className="name" aria-hidden="true">
+						hello!
+					</h1>
+					<h1 className="name" aria-hidden="true">
+						I am
+					</h1>
+					<h1 className="name" id="me" aria-hidden="true">
+						Laura Nemeth
+					</h1>
+					<h1 id="accessible-header">Hello! I am Laura Nemeth</h1>
 				</div>
 				<p>
 					Let's collaborate on beautiful websites, apps, and more! I am
